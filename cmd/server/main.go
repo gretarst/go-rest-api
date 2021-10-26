a59cb0a510632d4f9ada11e03796a6ea7b23df9d
gmagnusson <gretar.magnusson@futurenet.com>
2021-10-26 22:29:02 +0100

    Changes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gretarst/go-rest-api/internal/comment"
+	"github.com/gretarst/go-rest-api/internal/database"
 	transportHTTP "github.com/gretarst/go-rest-api/internal/transport/http"
 )
 
@@ -13,7 +15,15 @@ type App struct{}
 func (app *App) Run() error {
 	fmt.Println("Setting up App")
 
-	handler := transportHTTP.NewHandler()
+	var err error
+	db, err := database.NewDatabase()
+	if err != nil {
+		return err
+	}
+
+	commentService := comment.NewService(db)
+
+	handler := transportHTTP.NewHandler(commentService)
 	handler.SetupRoutes()
 
 	if err := http.ListenAndServe(":8080", handler.Router); err != nil {

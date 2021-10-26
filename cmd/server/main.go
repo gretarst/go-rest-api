2490274a284386a76603defe91e4240f3faa1d80
gmagnusson <gretar.magnusson@futurenet.com>
2021-10-26 15:35:24 +0100

    Application Structure

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// App - Struct which contains pointers to dependencies
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up App")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting REST API")
+		fmt.Println(err)
+	}
 }

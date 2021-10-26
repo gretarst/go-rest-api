aea10f332bd357f11248e7f254900e3c52d42b71
gmagnusson <gretar.magnusson@futurenet.com>
2021-10-26 22:28:18 +0100

    Working on it

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -3,26 +3,62 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/gretarst/go-rest-api/internal/comment"
 )
 
 // Stores Pointer to our services
 type Handler struct {
-	Router *mux.Router
+	Router  *mux.Router
+	Service *comment.Service
 }
 
 // Returns a pointer to a Handler
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(service *comment.Service) *Handler {
+	return &Handler{
+		Service: service,
+	}
 }
 
 // Setups the routes of the application
 func (h *Handler) SetupRoutes() {
 	fmt.Println("Setting up Routes")
 	h.Router = mux.NewRouter()
+
+	h.Router.HandleFunc("/api/comment", h.GetAllComments).Methods("GET")
+	h.Router.HandleFunc("/api/comment", h.PostComment).Methods("POST")
+	h.Router.HandleFunc("/api/comment/{id}", h.GetComment).Methods("GET")
+	h.Router.HandleFunc("/api/comment/{id}", h.UpdateComment).Methods("PUT")
+	h.Router.HandleFunc("/api/comment/{id}", h.DeleteComment).Methods("DELETE")
+
 	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "I am alive!")
 	})
 
+}
+
+func (h *Handler) GetComment(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	i, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		fmt.Fprintf(w, "Unable parse comment id")
+	}
+
+	comment, err := h.Service.GetComment(uint(i))
+	if err != nil {
+		fmt.Fprintf(w, "Error retrieving comment")
+	}
+
+	fmt.Fprintf(w, "%+v", comment)
+}
+
+func (h *Handler) GetAllComments(w http.ResponseWriter, r *http.Request) {
+	comment, err := h.Service.GetAllComments()
+	if err != nil {
+
+	}
 }
